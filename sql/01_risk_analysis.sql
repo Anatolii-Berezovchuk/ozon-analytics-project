6e75ec90b5ff3a0c8696a024daba94997746bda7
Tol_Ber <berezovchuk1010@mail.ru>
2026-03-26 23:02:13 +0300

    Update 01_risk_analysis.sql

--- sql/01_risk_analysis.sql
+++ sql/01_risk_analysis.sql
@@ -1,1 +1,16 @@
+SELECT job_role, industry, automation_risk_percent
+FROM ai_jobs
+ORDER BY automation_risk_percent DESC
+LIMIT 20;
 
+
+SELECT industry, AVG(automation_risk_percent) AS avg_risk
+FROM ai_jobs
+GROUP BY industry
+ORDER BY avg_risk DESC;
+
+
+SELECT country, AVG(automation_risk_percent) AS avg_risk
+FROM ai_jobs
+GROUP BY country
+ORDER BY avg_risk DESC;
